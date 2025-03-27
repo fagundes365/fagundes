6455d74ae8082760b2304278f4223b1a292a932d
rianfagundes <rianfagundesdev@outlook.com>
2025-03-27 20:00:54 -0300

    Update sorte.js

--- sorte.js
+++ sorte.js
@@ -5,8 +5,10 @@ let msg = [
     'Mantenha o foco, FACA NA CAVEIRA!!!',
     'Acredite no tempo certo que Deus tem para você',
     'Eu sei que você vencerá qualquer desafio, basta ter disciplina'
+];
 if (nome) {
     let randomMsg = msg[Math.floor(Math.random() * msg.length)];
+    alert(`Olá, ${nome}! ${randomMsg}!`);
 } else {
     let name = prompt("Digite SEU NOME:");
     let randomMsg = msg[Math.floor(Math.random() * msg.length)];
